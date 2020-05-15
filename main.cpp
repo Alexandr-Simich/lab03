7cf496a1c63b203e2efe6f93ed9d96231e38b220
Alexandr-Simich <simi55@mail.ru>
2020-05-16 00:25:33 +0300

    3.2.7

--- main.cpp
+++ main.cpp
@@ -9,18 +9,19 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     if (argc > 1)
-                            {
-                                CURL *curl = curl_easy_init();
-                        if(curl) {
-                          CURLcode res;
-                          curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
-                          res = curl_easy_perform(curl);
-                          curl_easy_cleanup(curl);
-                        }
-
-
-      //  cout << "argñ = " << argc;
-      //  cout << "argv[0] = "<< argv[0];
+    {
+                    CURL *curl = curl_easy_init();
+            if(curl)
+            {
+                CURLcode res;
+                curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+                res = curl_easy_perform(curl);
+                if (res) {
+                cerr << curl_easy_strerror(res);
+                exit(1);
+                }
+                curl_easy_cleanup(curl);
+            }
         return 0;
     }
 
