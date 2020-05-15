d446f143821808ab08d6319718ab758d4e2b083d
Alexandr-Simich <simi55@mail.ru>
2020-05-15 23:56:58 +0300

    3.2.6

--- main.cpp
+++ main.cpp
@@ -9,13 +9,21 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     if (argc > 1)
-    {
-        cout << "argñ = " << argc;
-        cout << "argv[0] = "<< argv[0];
+                            {
+                                CURL *curl = curl_easy_init();
+                        if(curl) {
+                          CURLcode res;
+                          curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+                          res = curl_easy_perform(curl);
+                          curl_easy_cleanup(curl);
+                        }
+
+
+      //  cout << "argñ = " << argc;
+      //  cout << "argv[0] = "<< argv[0];
         return 0;
     }
-    else
-    {
+
         curl_global_init(CURL_GLOBAL_ALL);
         Input input = read_input(cin, true);
         double min;
@@ -26,6 +34,9 @@ int main(int argc, char* argv[])
 
         show_histogram_svg(bins);
 
+
+
+
         return 0;
-    }
 }
+
