0364380bbd2bd10452d6b4f579d5d485ffa55b2b
Alexandr-Simich <simi55@mail.ru>
2020-05-29 21:36:43 +0300

    3.2

--- main.cpp
+++ main.cpp
@@ -1,102 +1,109 @@
+#include <iostream>
+#include <vector>
 #include "histogram.h"
 #include "svg.h"
 #include <windows.h>
-#include <curl/curl.h>
-#include <sstream>
-#include <string>
 using namespace std;
 
 DWORD WINAPI GetVersion(void);
 
-Input read_input(istream& in, bool prompt)
-{
-    Input data;
-    size_t number_count;
-    if(prompt)
-    {
-        cerr << "Enter number count: ";
-        in >> number_count;
-        cerr << "Enter numbers: ";
-        data.numbers = input_numbers(in, number_count);
-        cerr << "Enter column count: ";
-        in >> data.bin_count;
+vector<double>
+input_numbers(size_t count) {
+    vector<double> result(count);
+    for (size_t i = 0; i < count; i++) {
+        cin >> result[i];
     }
-    else
-    {
-        in >> number_count;
-        data.numbers = input_numbers(in, number_count);
-        in >> data.bin_count;
-    }
-
-    return data;
-}
-size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx)
-{
-    const size_t data_size = item_size * item_count;
-    const char* new_items = reinterpret_cast<const char*>(items);
-    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
-    buffer->write(new_items, data_size);
-    return data_size;
+    return result;
 }
 
-Input download(const string& address)
-{
-    stringstream buffer;
-
-    curl_global_init(CURL_GLOBAL_ALL);
-
-    CURL *curl = curl_easy_init();
-    if(curl)
+vector <size_t>
+make_histogram(vector <double> numbers,size_t bin_count,double min,double max){
+    vector<size_t> bins(bin_count);
+    for (double number : numbers)
     {
-        CURLcode res;
-        curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
-        res = curl_easy_perform(curl);
-        if (res)
+        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
+        if (bin == bin_count)
         {
-            cout << curl_easy_strerror(res) << endl;
-            exit(1);
+            bin--;
+        }
+        bins[bin]++;
+    }
+    return(bins);
+}
+
+void
+show_histogram_text(vector<size_t>bins){
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+
+    size_t max_count = 0;
+    for (size_t count : bins)
+    {
+        if (count > max_count)
+        {
+            max_count = count;
         }
     }
-    curl_easy_cleanup(curl);
-    return read_input(buffer, false);
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+
+    for (size_t bin : bins)
+    {
+        if (bin < 100)
+        {
+            cout << ' ';
+        }
+        if (bin < 10)
+        {
+            cout << ' ';
+        }
+        cout << bin << "|";
+
+        size_t height = bin;
+        if (scaling_needed)
+        {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            height = (size_t)(bin * scaling_factor);
+        }
+
+        for (size_t i = 0; i < height; i++)
+        {
+            cout << '*';
+        }
+        cout << '\n';
+    }
 }
 
-int main(int argc, char* argv[])
-{
-    printf("Version is  %x\n", GetVersion());
-    printf("Version is  %u\n", GetVersion());
+int main() {
+    DWORD mask = 0x0000ffff;
+    DWORD mask_major = 0b00000000'00000000'00000000'11111111;
+    DWORD info = GetVersion();
+    DWORD platform = info >> 16;
+    DWORD version = info & mask;
+    DWORD version_major = version & mask_major;
+    DWORD version_minor = version >> 8;
+    printf("Windows (10) version is %u.\n", version);
+    printf("Windows (16) version is %x.\n", version);
+    printf("Platform is %u.\n", platform);
+    printf("Windows major version is %u.\n", version_major);
+    printf("Windows minor version is %u.\n", version_minor);
 
-  /*  Input input;
-    if (argc > 1)
-    {
-        input = download(argv[1]);
+    if ((info & 0x40000000) == 0) {
+        DWORD build = platform;
+        printf("Windows build is %u.\n", build);
     }
-    else
-    {
-        input = read_input(cin, true);
-    }
-    double min = 0, max = 0;
-    find_minmax(input, min, max);
-    const auto bins = make_histogram(input, min, max);
-    show_histogram_svg(bins);
+    printf("Windows v%u.%u (build %u)\n",version_major,version_minor,platform);
+    return 0;
+    /*size_t number_count;
+    cerr << "Enter number count: ";
+    cin >> number_count;
+    cerr << "Enter numbers: ";
+    const auto numbers = input_numbers(number_count);
+    size_t bin_count;
+    cerr << "Enter column count: ";
+    cin >> bin_count;
+    double min, max;
+    find_minmax(numbers, min, max);
+    const auto bins = make_histogram(numbers, bin_count,min,max);
+    show_histogram_svg(bins); */
 
-    return 0; */
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//const vector<double>& numbers,
-//  size_t& bin_count, double min, double max
